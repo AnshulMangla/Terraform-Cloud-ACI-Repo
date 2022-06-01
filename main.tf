f67846bca2b8ef0ea7d5c7e9d9b098db79cc86be
AnshulMangla <87487619+AnshulMangla@users.noreply.github.com>
2022-06-01 11:07:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "aci_tenant" "anshul_terraform" {
 }
 
 # Define an ACI Tenant VRF Resource.
-resource "aci_vrf" "terraform_vrf" {
-    tenant_dn   = aci_tenant.anshul_terraform.id
-    description = "VRF Modified Using Terraform"
-    name        = "anshul_vrf2"
-}
+#resource "aci_vrf" "terraform_vrf" {
+#    tenant_dn   = aci_tenant.anshul_terraform.id
+#    description = "VRF Modified Using Terraform"
+#    name        = "anshul_vrf2"
+#}
