5e037eef0e24663896bb865d92a96692bddd85d6
AnshulMangla <87487619+AnshulMangla@users.noreply.github.com>
2022-07-14 13:12:12 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,19 +16,19 @@ terraform {
 
 # Provider Configuration
 provider "aci" {
-    url = "https://sandboxapicdc.cisco.com"
-    username = "admin"
-    password = "!v3G@!4@Y"
+    url = "https://10.106.236.56"
+    username = "pandapa"
+    password = "cisco!123"
 }
 
 # ACI Tenant
-#resource "aci_tenant" "anshul_terraform" {
-#    name = "anshul_terraform"
-#}
+resource "aci_tenant" "anshul_terraform" {
+    name = "anshul_terraform"
+}
 
 # Define an ACI Tenant VRF Resource.
-#resource "aci_vrf" "terraform_vrf" {
-#    tenant_dn   = aci_tenant.anshul_terraform.id
-#    description = "VRF Modified Using Terraform"
-#    name        = "anshul_vrf2"
-#}
+ resource "aci_vrf" "terraform_vrf" {
+    tenant_dn   = aci_tenant.anshul_terraform.id
+    description = "VRF Modified Using Terraform"
+    name        = "anshul_vrf2"
+}
